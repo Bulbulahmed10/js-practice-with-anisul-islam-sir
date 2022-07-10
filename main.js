a940a3309e494470303d255a78314bc2eb29fe39
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-07-10 09:19:57 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+
+// alert('BUlbul Ahmed')
+// document.write('I love Myself')
+// console.log('I Love finalnd') 
+
+// var num = 2.5648
+// console.log(num.toPrecision(3))
+
+// var text =('Bangladesh')
+
+// document.write(text.charAt(2))
+// text = text.toUpperCase()
+// document.write(text)
+
+// var text1 = text.slice(2, 5)
+// document.write(text1)
+
+// var firstName = 'Bulbul '
+// var lastName = ' Ahmed'
+
+// var finalName = document.write(firstName +  lastName);
+// document.write(finalName) 
+
+
+/*
+ Calculator Project 
+ */
+
+var num1 = prompt('Enter first number : ')
+var num2 = prompt('Enter second number : ')
+
+num1 = parseInt(num1, 10)
+num2 = parseInt(num2, 10)
+
+
+
+sum = num1 + num2;
+document.write(num1 + ' + ' + num2 + ' = '+sum + '</br>')
+
+sub = num1 - num2;
+document.write(num1 + ' - ' + num2 + ' = '+sub + '</br>')
+
+multiplication = num1 * num2
+document.write(num1 + ' * ' + num2 + ' = '+multiplication + '</br>')
+
+division = num1 / num2
+document.write(num1 + ' / ' + num2 + ' = '+division + '</br>')
