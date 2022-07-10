90e05b922e8492f15590c2d62abc150c8800f34c
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-07-10 16:24:26 +0600

    added area of shapes, letter grading and identify large number

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 // alert('BUlbul Ahmed')
 // document.write('I love Myself')
-// console.log('I Love finalnd') 
+// console.log('I Love finalnd')
 
 // var num = 2.5648
 // console.log(num.toPrecision(3))
@@ -19,32 +18,96 @@
 // var lastName = ' Ahmed'
 
 // var finalName = document.write(firstName +  lastName);
-// document.write(finalName) 
-
+// document.write(finalName)
 
 /*
  Calculator Project 
  */
 
-var num1 = prompt('Enter first number : ')
-var num2 = prompt('Enter second number : ')
+// var num1 = prompt('Enter first number : ')
+// var num2 = prompt('Enter second number : ')
 
-num1 = parseInt(num1, 10)
-num2 = parseInt(num2, 10)
+// num1 = parseInt(num1, 10)
+// num2 = parseInt(num2, 10)
 
+// sum = num1 + num2;
+// document.write(num1 + ' + ' + num2 + ' = '+sum + '</br>')
 
+// sub = num1 - num2;
+// document.write(num1 + ' - ' + num2 + ' = '+sub + '</br>')
 
-sum = num1 + num2;
-document.write(num1 + ' + ' + num2 + ' = '+sum + '</br>')
+// multiplication = num1 * num2
+// document.write(num1 + ' * ' + num2 + ' = '+multiplication + '</br>')
 
-sub = num1 - num2;
-document.write(num1 + ' - ' + num2 + ' = '+sub + '</br>')
+// division = num1 / num2
+// document.write(num1 + ' / ' + num2 + ' = '+division + '</br>')
 
-multiplication = num1 * num2
-document.write(num1 + ' * ' + num2 + ' = '+multiplication + '</br>')
+// rem = num1 % num2
+// document.write(num1 + ' % ' + num2 + ' = '+rem + '</br>')
 
-division = num1 / num2
-document.write(num1 + ' / ' + num2 + ' = '+division + '</br>')
+/*
+ area of shapes
+ */
 
-rem = num1 % num2
-document.write(num1 + ' % ' + num2 + ' = '+rem + '</br>')
+ var base = parseFloat(prompt('Enter First Input :'))
+ var height = parseFloat(prompt('Enter Second Input :'))
+
+ square = base * height
+ document.write('Total Square Area' + ' = ' + square + '</br>')
+
+ rectangle = base * height
+ document.write('Total Rectangle Area' + ' = ' + rectangle + '</br>')
+
+ triangle = (base * height) / 2
+ document.write('Total Square Area' + ' = ' + triangle + '</br')
+
+ var trapezoidBase1 = parseFloat(prompt('Please enter base1 of trapezoid :'))
+ var trapezoidBase2 = parseFloat(prompt('Please enter base2 of trapezoid :'))
+ var trapezoidHeight = parseFloat(prompt('Please enter height of trapezoid :'))
+
+ areaOfTrapezoid = (trapezoidBase1 + trapezoidBase2) * trapezoidHeight /2
+ document.write('Total Trapezoid Area' + ' = ' + areaOfTrapezoid + '</br')
+
+ var farn = parseFloat(prompt('Enter Fahrenheit :'))
+
+ cels = (farn-32) * (5/9)
+
+ document.write('celsius : ' + cels)
+
+/*
+ letter grading
+ */
+
+var num = parseInt(prompt("Enter Result Number"));
+
+if (num >= 80) {
+  document.write("You Got : A+");
+} else if (num >= 70) {
+  document.write("You Got : A");
+} else if (num >= 60) {
+  document.write("You Got : A-");
+} else if (num >= 50) {
+  document.write("You Got : B");
+} else if (num >= 40) {
+  document.write("You Got: C");
+} else if (num >= 33) {
+  document.write("You Got : D");
+} else if (num > 100 || num < 0) {
+  document.write("Invalid Marks");
+} else {
+  document.write("You Fail");
+}
+
+/*
+ identify large number
+ */
+
+var num1 = prompt("Enter Number 1 : ");
+var num2 = prompt("Enter Number 2 : ");
+var num3 = prompt("Enter Number 3 : ");
+
+if (num1 > num2 && num1 > num3) {
+  document.write("Large Number 1 = "  + num1);
+} else if (num2 > num1 && num2 > num3) {
+  document.write("Large Number 2 = "  + num2);
+} else document.write("Large Number 3 = "  + num3)
